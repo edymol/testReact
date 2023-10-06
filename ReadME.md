71af4296a7ec4386d4c20eb92cbdf0bbf4ff2738
edymol Github <edymolinap@gmail.com>
2023-10-06 15:28:21 -0700

    React practice test

---
+++ ReadME.md
@@ -0,0 +1,11 @@
+# How to stady for a React technical interview
+
+APIs:
+
+- Facts Random: https://catfact.ninja/fact
+- Image Random: https://cataas.com/cat/says/hello
+
+- fetch a fact about cats with the first API and show a cat image with the first word of the cat's fact using the second API.
+ - 1. Get the cat's fact
+ - 2. Get the cat's image
+ - 3. Show the cat's image with the first word of the fact
---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const CAT_FACT_RANDOM = "https://catfact.ninja/fact";
+// const CAT_IMAGE_URL = `https://cataas.com/cat/says/{firstWord}?size=50&color=red&json=true`;
+
+const CAT_IMAGE_URL = "https://cataas.com/";
+export function App() {
+    const [fact, setFact] = useState("fact cat fetch fact");
+    const [imageUrl, setImageUrl] = useState("");
+
+    // this effect runs when the fact is gathered
+    useEffect(() => {
+        // async function getCatFact() {
+        //     const res = await fetch(CAT_FACT_RANDOM);
+        //     const data = await res.json();
+        //     setFact(data.fact);
+        // }
+        fetch(CAT_FACT_RANDOM)
+            .then((response) => response.json())
+            .then((data) => {
+                const { fact } = data;
+                setFact(fact);
+            });
+    }, []);
+
+    // this effect runs when the image is gathered
+
+    useEffect(() => {
+        if (!fact) return;
+
+        const firstWords = fact.split(" ", 5).join(" ");
+        console.log(firstWords);
+
+        fetch(
+            `https://cataas.com/cat/says/${firstWords}?size=50&color=red&json=true`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                const { url } = data;
+                setImageUrl(url);
+            });
+    }, [fact]);
+
+    return (
+        <main>
+            <h1>Kitten app</h1>
+            {fact && <p>{fact}</p>}
+            {imageUrl && (
+                <img
+                    src={`${CAT_IMAGE_URL}${imageUrl}`}
+                    alt={`Image fetch from API ${fact}`}
+                />
+            )}
+        </main>
+    );
+}
