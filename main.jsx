51a1f2622c66a74f61aec633adc91b65d817dc9b
edymol Github <edymolinap@gmail.com>
2023-10-07 13:34:13 -0700

    feature: add counter

--- main.jsx
+++ main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './src/app.jsx';
 import { ToDo } from './src/ToDo.jsx';
+import { Counter } from './src/Counter.jsx'; 
 
 const root = createRoot(document.getElementById('app'));
 root.render(
   <>
+    
     <App />
+    <Counter />
     <ToDo />
   </>
 );
---
+++ src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function Counter() {
+    const [ count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleClick}>Increase count</button>
+        </div>
+    )
+}
